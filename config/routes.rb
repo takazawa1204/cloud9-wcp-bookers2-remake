e7bc97045184190c04d8578d68947d240b94ac26
takazawa1204 <ckicken1204@gmail.com>
2021-09-07 11:42:39 +0000

    [Finish]3-0907

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books do
+  resources :books,only: [:show, :index, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    
+    resources :book_comments, only: [:create, :destroy]
   end
-  
   
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
