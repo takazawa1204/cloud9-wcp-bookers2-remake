cfd284ceb0f60d807c5b585ad44a4ddf0bf8afbc
takazawa1204 <ckicken1204@gmail.com>
2021-09-08 02:31:42 +0000

    [Finish]4-0908

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books,only: [:show, :index, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     
